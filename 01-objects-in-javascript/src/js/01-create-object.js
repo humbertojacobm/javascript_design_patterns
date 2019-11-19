1539867e675984d3ad1429b142ad37bb6f4b5a32
humbertojacobm <humbertojacobm@gmail.com>
2019-11-19 10:41:16 -0500

    module: object in javascript -> demo define property

--- 01-objects-in-javascript/src/js/01-create-object.js
+++ 01-objects-in-javascript/src/js/01-create-object.js
@@ -1,12 +1,30 @@
 console.log("how to create an object");
 
-var task = Object.create(Object.prototype);
+//var task = Object.create(Object.prototype);
 //var task = new Object();
-
-task.title = 'My title';
-task.description = "My description";
-task.toString = function(){
-    return `${this.title} - ${this.description}`;
+var task = {
+    title : 'My title',
+    description : "My description"
 }
 
-console.log(task.toString());
+Object.defineProperty(task,'toString',{
+   value: function(){
+    return `${this.title} - ${this.description}`;
+   },
+   writable: false,
+   enumerable: false,
+   configurable: false
+});
+
+// Object.defineProperty(task,'toString',{
+//    enumerable:true
+// });
+
+//task.toString = 'hi';
+
+// task.toString = function(){
+//     return `${this.title} - ${this.description}`;
+// }
+
+//console.log(task.toString());
+console.log(Object.keys(task));
